4c4a497d5925df13170d70c32d9ecd76d4f6c17a
IDoneShaveIt <idanshavit31@gmail.com>
2023-12-12 00:35:30 +0200

    add marketing DAG

---
+++ jaffle_shop_online/models/marketing/agg_sessions.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized = "incremental",
+        unique_key = "session_id",
+    )
+}}
+
+with app_sessions as (
+    select *
+    from {{ source("sessions", "stg_app_sessions") }}
+),
+
+website_sessions as (
+    select *
+    from {{ source("sessions", "stg_website_sessions") }}
+)
+
+select *, 'app' as platform
+from app_sessions
+union all
+select *, 'website' as platform
+from website_sessions
---
+++ jaffle_shop_online/models/marketing/marketing_ads.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized = "incremental",
+        unique_key = "ad_id",
+    )
+}}
+
+with google_ads as (
+    select *
+    from {{ source("ads", "stg_google_ads") }}
+),
+
+facebook_ads as (
+    select *
+    from {{ source("ads", "stg_facebook_ads") }}
+),
+
+instagram_ads as (
+    select *
+    from {{ source("ads", "stg_instagram_ads") }}
+)
+
+select *, 'google' as utm_source
+from google_ads
+union all
+select *, 'facebook' as utm_source
+from facebook_ads
+union all
+select *, 'instagram' as utm_source
+from instagram_ads
