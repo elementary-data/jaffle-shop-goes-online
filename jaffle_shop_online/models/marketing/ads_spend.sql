42354d0952c14e653d62ed405fbd03541dbee9be
MikaKerman <mika.kerman@gmail.com>
2025-06-29 19:59:32 +0300

    Fix date formatting in ads spend SQL query for consistency

--- jaffle_shop_online/models/marketing/ads_spend.sql
+++ jaffle_shop_online/models/marketing/ads_spend.sql
@@ -9,6 +9,6 @@ with marketing_ads as (
     from {{ ref("marketing_ads") }}
 )
 
-select date as date_day, utm_source, utm_medium, utm_campain, sum(cost) as spend
+select date(date) as date_day, utm_source, utm_medium, utm_campain, sum(cost) as spend
 from marketing_ads
-group by date, utm_source, utm_medium, utm_campain
+group by date(date), utm_source, utm_medium, utm_campain
